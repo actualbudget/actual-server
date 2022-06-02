332f111aa4663a26b81dd973875cbf7874dabd58
KatrovMan <culver.richard@icloud.com>
2022-06-02 11:15:26 -0400

    WIP

--- sql/account.sql
+++ sql/account.sql
@@ -15,15 +15,3 @@ CREATE TABLE files
    encrypt_test TEXT,
    deleted BOOLEAN DEFAULT FALSE,
    name TEXT);
-
-CREATE TABLE webTokens
-   (user_id TEXT PRIMARY KEY,
-   token_id TEXT,
-   time_created TEXT,
-   contents TEXT);
-
-CREATE TABLE access_tokens
-   (item_id TEXT PRIMARY KEY,
-   user_id TEXT,
-   access_token TEXT,
-   deleted BOOLEAN);
