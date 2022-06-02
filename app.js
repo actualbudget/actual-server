082cc44bd37395b6004d7e596fe893ebd062a126
KatrovMan <culver.richard@icloud.com>
2022-06-02 10:48:55 -0400

    WIP

--- app.js
+++ app.js
@@ -61,7 +61,6 @@ async function run() {
 
   console.log('Listening on ' + config.hostname + ':' + config.port + '...');
   app.listen(config.port, config.hostname);
-  console.log(process.env);
 }
 
 run().catch(err => {
