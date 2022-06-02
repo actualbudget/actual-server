66adb059626c014666800c3548a564a03ab6e40a
KatrovMan <culver.richard@icloud.com>
2022-06-02 10:38:56 -0400

    ENV variables on fly.io

--- app-plaid.js
+++ app-plaid.js
@@ -20,9 +20,9 @@ async function init() {
 
   // if (rows.length !== 0) {
     plaidClient = new plaid.Client({
-      clientID: process.env.PLAID_CLIENT_ID,
-      secret: process.env.PLAID_SECRET,
-      env: plaid.environments[process.env.PLAID_ENV],
+      clientID: $PLAID_CLIENT_ID,
+      secret: $PLAID_SECRET,
+      env: plaid.environments[$PLAID_ENV],
       options: { version: '2019-05-29' }
     });
   // }
