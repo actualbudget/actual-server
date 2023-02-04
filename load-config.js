be36c2745d7598184af1f4da26b8750396a7b167
Jed Fox <git@jedfox.com>
2023-02-04 16:51:03 -0500

    Fix TS

--- load-config.js
+++ load-config.js
@@ -1,15 +1,16 @@
-let config = {};
+let userConfig;
 let fs = require('fs');
 let { join } = require('path');
 let root = fs.existsSync('/data') ? '/data' : __dirname;
 
 try {
   // @ts-expect-error TS2307: we expect this file may not exist
-  config = require('./config');
+  userConfig = require('./config');
 } catch (e) {
   // do nothing
 }
 
+/** @type {Omit<import('./config-types').Config, 'mode'>} */
 let defaultConfig = {
   port: 5006,
   hostname: '::',
@@ -17,6 +18,9 @@ let defaultConfig = {
   userFiles: join(root, 'user-files'),
   webRoot: join(__dirname, 'node_modules', '@actual-app', 'web', 'build')
 };
+
+/** @type {import('./config-types').Config} */
+let config;
 if (process.env.NODE_ENV === 'test') {
   config = {
     mode: 'test',
@@ -26,21 +30,23 @@ if (process.env.NODE_ENV === 'test') {
   config = {
     mode: 'development',
     ...defaultConfig,
-    ...config
+    ...userConfig
   };
 }
 
-// The env variable always takes precedence
-config.port = process.env.ACTUAL_PORT || process.env.PORT || config.port;
-config.hostname = process.env.ACTUAL_HOSTNAME || config.hostname;
-config.serverFiles = process.env.ACTUAL_SERVER_FILES || config.serverFiles;
-config.userFiles = process.env.ACTUAL_USER_FILES || config.userFiles;
-config.webRoot = process.env.ACTUAL_WEB_ROOT || config.webRoot;
-if (process.env.ACTUAL_HTTPS_KEY && process.env.ACTUAL_HTTPS_CERT) {
-  config.https = {
-    key: process.env.ACTUAL_HTTPS_KEY,
-    cert: process.env.ACTUAL_HTTPS_CERT
-  };
-}
-
-module.exports = config;
+module.exports = {
+  ...config,
+  port: +process.env.ACTUAL_PORT || +process.env.PORT || config.port,
+  hostname: process.env.ACTUAL_HOSTNAME || config.hostname,
+  serverFiles: process.env.ACTUAL_SERVER_FILES || config.serverFiles,
+  userFiles: process.env.ACTUAL_USER_FILES || config.userFiles,
+  webRoot: process.env.ACTUAL_WEB_ROOT || config.webRoot,
+  https:
+    process.env.ACTUAL_HTTPS_KEY && process.env.ACTUAL_HTTPS_CERT
+      ? {
+          key: process.env.ACTUAL_HTTPS_KEY,
+          cert: process.env.ACTUAL_HTTPS_CERT,
+          ...config.https
+        }
+      : config.https
+};
