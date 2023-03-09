3309ef3ddccfb7adf737b3a851f3f129428a04e2
Matiss Janis Aboltins <matiss@mja.lv>
2023-03-09 21:20:12 +0000

    Feedback

--- src/load-config.js
+++ src/load-config.js
@@ -68,10 +68,13 @@ export default {
           ...(config.https || {}),
         }
       : config.https,
-  nordigen: {
-    secretId:
-      process.env.ACTUAL_NORDIGEN_SECRET_ID || config.nordigen?.secretId,
-    secretKey:
-      process.env.ACTUAL_NORDIGEN_SECRET_KEY || config.nordigen?.secretKey,
-  },
+  nordigen:
+    process.env.ACTUAL_NORDIGEN_SECRET_ID &&
+    process.env.ACTUAL_NORDIGEN_SECRET_KEY
+      ? {
+          secretId: process.env.ACTUAL_NORDIGEN_SECRET_ID,
+          secretKey: process.env.ACTUAL_NORDIGEN_SECRET_KEY,
+          ...(config.nordigen || {}),
+        }
+      : config.nordigen,
 };
