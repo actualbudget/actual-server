4851b0b698fd886c55e5253679921d0100d79bb3
Jed Fox <git@jedfox.com>
2023-04-04 14:29:49 -0400

    Add some basic debug logging

--- src/load-config.js
+++ src/load-config.js
@@ -1,10 +1,17 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import createDebug from 'debug';
+
+const debug = createDebug('actual:config');
+const debugSensitive = createDebug('actual-sensitive:config');
 
 const projectRoot = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
+debug(`project root: '${projectRoot}'`);
 export const sqlDir = path.join(projectRoot, 'src', 'sql');
+
 let defaultDataDir = fs.existsSync('/data') ? '/data' : projectRoot;
+debug(`default data directory: '${defaultDataDir}'`);
 
 function parseJSON(path, allowMissing = false) {
   let text;
@@ -12,6 +19,7 @@ function parseJSON(path, allowMissing = false) {
     text = fs.readFileSync(path, 'utf8');
   } catch (e) {
     if (allowMissing) {
+      debug(`config file '${path}' not found, ignoring.`);
       return {};
     }
     throw e;
@@ -21,8 +29,12 @@ function parseJSON(path, allowMissing = false) {
 
 let userConfig;
 if (process.env.ACTUAL_CONFIG_PATH) {
+  debug(
+    `loading config from ACTUAL_CONFIG_PATH: '${process.env.ACTUAL_CONFIG_PATH}'`,
+  );
   userConfig = parseJSON(process.env.ACTUAL_CONFIG_PATH);
 } else {
+  debug(`loading config from default path: '${defaultDataDir}/config.json'`);
   userConfig = parseJSON(path.join(defaultDataDir, 'config.json'), true);
 }
 
@@ -58,7 +70,7 @@ if (process.env.NODE_ENV === 'test') {
   };
 }
 
-export default {
+const config = {
   ...config,
   port: +process.env.ACTUAL_PORT || +process.env.PORT || config.port,
   hostname: process.env.ACTUAL_HOSTNAME || config.hostname,
@@ -83,3 +95,31 @@ export default {
         }
       : config.nordigen,
 };
+
+debug(`using port ${config.port}`);
+debug(`using hostname ${config.hostname}`);
+debug(`using server files directory ${config.serverFiles}`);
+debug(`using user files directory ${config.userFiles}`);
+debug(`using web root directory ${config.webRoot}`);
+
+if (config.https) {
+  debug(`using https key: ${'*'.repeat(config.https.key.length)}`);
+  debugSensitive(`using https key ${config.https.key}`);
+  debug(`using https cert: ${'*'.repeat(config.https.cert.length)}`);
+  debugSensitive(`using https cert ${config.https.cert}`);
+}
+
+if (config.nordigen) {
+  debug(
+    `using nordigen secret id: ${'*'.repeat(config.nordigen.secretId.length)}`,
+  );
+  debugSensitive(`using nordigen secret id ${config.nordigen.secretId}`);
+  debug(
+    `using nordigen secret key: ${'*'.repeat(
+      config.nordigen.secretKey.length,
+    )}`,
+  );
+  debugSensitive(`using nordigen secret key ${config.nordigen.secretKey}`);
+}
+
+export default config;
