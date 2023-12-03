bac62948c8ed3e3415f9bf21459614e152734b48
pproenca <8202400+pproenca@users.noreply.github.com>
2023-12-03 18:18:43 +0000

    latest

--- src/app-gocardless/banks/monzo-monzgb2l.js
+++ src/app-gocardless/banks/monzo-monzgb2l.js
@@ -1,4 +1,4 @@
-import { FIELD_PATTERNS } from '../patterns/monzo.js';
+import { FIELD_PATTERNS, TRANSACTION_CODES } from '../patterns/monzo.js';
 import { VENDOR_PATTERNS } from '../patterns/vendors.js';
 import {
   applyTransactionPatterns as applyTransactionPatterns,
@@ -6,35 +6,6 @@ import {
   normalizeCreditorAndDebtorNames,
 } from '../util/apply-pattern.js';
 import * as ib from './integration-bank.js';
-
-const TRANSACTION_CODES = {
-  '3dsecure': 'Online security-authenticated transaction',
-  account_interest: 'Account interest credit',
-  bacs: 'Regular BACS payment',
-  card_delivery: 'Bank card delivery charge',
-  chaps: 'High-value same-day transfer',
-  collections_settlement: 'Collections or debt settlement',
-  emergency_cash: 'Emergency cash withdrawal',
-  faster_payments: 'Immediate Faster Payments transfer',
-  instalment_loan: 'Instalment loan repayment',
-  ledger_adjustment: 'Ledger correction adjustment',
-  locked_money: 'Locked funds transaction',
-  mastercard: 'Mastercard network transaction',
-  monzo_business_account_billing: 'Monzo business account service billing',
-  monzo_flex: 'Monzo Flex plan transaction',
-  monzo_paid: 'Monzo service payment',
-  overdraft: 'Overdraft transaction',
-  p2p_payment: 'Peer-to-peer payment',
-  payport_faster_payments: 'Payport Faster Payments transaction',
-  rbs_cheque: 'RBS cheque transaction',
-  sepa: 'SEPA network transaction',
-  signup_referral: 'Signup referral transaction',
-  spread_the_cost: 'Cost-spreading service transaction',
-  topup: 'Account top-up',
-  uk_business_pot: 'UK business financial transaction',
-  uk_cash_deposits_paypoint: 'UK PayPoint cash deposit',
-  // uk_retail_pot: 'UK retail financial transaction',
-};
 
 export default {
   institutionIds: ['MONZO_MONZGB2L'],
--- src/app-gocardless/patterns/monzo.js
+++ src/app-gocardless/patterns/monzo.js
@@ -1,3 +1,32 @@
+export const TRANSACTION_CODES = {
+  '3dsecure': 'Online security-authenticated transaction',
+  account_interest: 'Account interest credit',
+  bacs: 'Regular BACS payment',
+  card_delivery: 'Bank card delivery charge',
+  chaps: 'High-value same-day transfer',
+  collections_settlement: 'Collections or debt settlement',
+  emergency_cash: 'Emergency cash withdrawal',
+  faster_payments: 'Immediate Faster Payments transfer',
+  instalment_loan: 'Instalment loan repayment',
+  ledger_adjustment: 'Ledger correction adjustment',
+  locked_money: 'Locked funds transaction',
+  mastercard: 'Mastercard network transaction',
+  monzo_business_account_billing: 'Monzo business account service billing',
+  monzo_flex: 'Monzo Flex plan transaction',
+  monzo_paid: 'Monzo service payment',
+  overdraft: 'Overdraft transaction',
+  p2p_payment: 'Peer-to-peer payment',
+  payport_faster_payments: 'Payport Faster Payments transaction',
+  rbs_cheque: 'RBS cheque transaction',
+  sepa: 'SEPA network transaction',
+  signup_referral: 'Signup referral transaction',
+  spread_the_cost: 'Cost-spreading service transaction',
+  topup: 'Account top-up',
+  uk_business_pot: 'UK business financial transaction',
+  uk_cash_deposits_paypoint: 'UK PayPoint cash deposit',
+  // uk_retail_pot: 'UK retail financial transaction',
+};
+
 export const FIELD_PATTERNS = [
   {
     transactionCode: 'mastercard',
