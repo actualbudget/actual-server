fc4505ff6491ce02c4ad8f6d5b3f5163b92676df
pproenca <8202400+pproenca@users.noreply.github.com>
2023-12-10 13:17:51 +0000

    latest

--- src/app-gocardless/bank-factory.js
+++ src/app-gocardless/bank-factory.js
@@ -1,18 +1,18 @@
 import AmericanExpressAesudef1 from './banks/american-express-aesudef1.js';
-import AmericanEexpressAesugb21 from './custom-banks/american-express-aesugb21.js';
 import Belfius from './banks/belfius_gkccbebb.js';
 import BnpBeGebabebb from './banks/bnp-be-gebabebb.js';
-import CaixaGeralDepositorsCgdPtPl from './custom-banks/caixa-geral-depositos-cgdiptpl.js';
 import DanskeBankDabNO22 from './banks/danskebank-dabno22.js';
 import IngPlIngbplpw from './banks/ing-pl-ingbplpw.js';
 import IntegrationBank from './banks/integration-bank.js';
 import MbankRetailBrexplpw from './banks/mbank-retail-brexplpw.js';
-import MonzoMonzGb21 from './custom-banks/monzo-monzgb2l.js';
 import NorwegianXxNorwnok1 from './banks/norwegian-xx-norwnok1.js';
-import RevolutRevoGb21 from './custom-banks/revolut-revogb21.js';
 import SandboxfinanceSfin0000 from './banks/sandboxfinance-sfin0000.js';
-import SantanderGbAbbyGb21 from './custom-banks/santander-gb-abbygb2l.js';
 import SparNordSpNoDK22 from './banks/sparnord-spnodk22.js';
+import AmericanEexpressAesugb21 from './custom-banks/american-express-aesugb21.js';
+import CaixaGeralDepositorsCgdPtPl from './custom-banks/caixa-geral-depositos-cgdiptpl.js';
+import MonzoMonzGb21 from './custom-banks/monzo-monzgb2l.js';
+import RevolutRevoGb21 from './custom-banks/revolut-revogb21.js';
+import SantanderGbAbbyGb21 from './custom-banks/santander-gb-abbygb2l.js';
 
 const banks = [
   AmericanEexpressAesugb21,
