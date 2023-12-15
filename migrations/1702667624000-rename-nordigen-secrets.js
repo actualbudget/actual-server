ea705460356a153c3859a5d0efcf17c1b1bf5b79
Matiss Janis Aboltins <matiss@mja.lv>
2023-12-15 19:23:57 +0000

    Typo

--- migrations/1702667624000-rename-nordigen-secrets.js
+++ migrations/1702667624000-rename-nordigen-secrets.js
@@ -5,7 +5,7 @@ export const up = async function () {
     `UPDATE secrets SET name = "gocardless_secretId" WHERE name = "nordigen_secretId"`,
   );
   await getAccountDb().exec(
-    `UPDATE secrets SET name = "gocardless_secretKey" WHERE name = "nordigen_secretKey" `,
+    `UPDATE secrets SET name = "gocardless_secretKey" WHERE name = "nordigen_secretKey"`,
   );
 };
 
@@ -14,6 +14,6 @@ export const down = async function () {
     `UPDATE secrets SET name = "nordigen_secretId" WHERE name = "gocardless_secretId"`,
   );
   await getAccountDb().exec(
-    `UPDATE secrets SET name = "nordigen_secretKey" WHERE name = "gocardless_secretKey" `,
+    `UPDATE secrets SET name = "nordigen_secretKey" WHERE name = "gocardless_secretKey"`,
   );
 };
