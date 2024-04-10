2e68a6deb102bb2fe459a284de582d2378e1304e
matt <matt@fiddaman.net>
2024-04-10 18:07:03 +0000

    Make running app.js directory agnostic

--- src/load-config.js
+++ src/load-config.js
@@ -33,6 +33,8 @@ if (process.env.ACTUAL_CONFIG_PATH) {
     `loading config from ACTUAL_CONFIG_PATH: '${process.env.ACTUAL_CONFIG_PATH}'`,
   );
   userConfig = parseJSON(process.env.ACTUAL_CONFIG_PATH);
+
+  defaultDataDir = userConfig.dataDir ?? defaultDataDir;
 } else {
   let configFile = path.join(projectRoot, 'config.json');
 
@@ -60,6 +62,7 @@ let defaultConfig = {
     syncEncryptedFileSizeLimitMB: 50,
     fileSizeLimitMB: 20,
   },
+  projectRoot,
 };
 
 /** @type {import('./config-types.js').Config} */
