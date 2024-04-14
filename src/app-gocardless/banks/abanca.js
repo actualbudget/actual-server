900494cefa7f34b7f6975a9a2609a74d0068e8a2
daca11 <daca11.n2o@gmail.com>
2024-04-14 16:43:31 +0200

    Get proper creditorName from Abanca transaction

---
+++ src/app-gocardless/banks/abanca.js
@@ -0,0 +1,26 @@
+import Fallback from './integration-bank.js';
+
+/** @type {import('./bank.interface.js').IBank} */
+export default {
+  institutionIds: ['ABANCA_XXX'], //TODO: get institution ID for Abanca
+
+  normalizeAccount(account) {
+    return Fallback.normalizeAccount(account);
+  },
+
+  // Abanca transactions doesn't get the creditorID properly
+  normalizeTransaction(transaction, _booked) {
+    return {
+      ...transaction,
+      creditorName: transaction.remittanceInformationStructured,
+    };
+  },
+
+  sortTransactions(transactions = []) {
+    return Fallback.sortTransactions(transactions);
+  },
+
+  calculateStartingBalance(sortedTransactions = [], balances = []) {
+    return Fallback.calculateStartingBalance(sortedTransactions, balances);
+  },
+};
