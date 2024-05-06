275942ffa82c3fca29c2f617b3aa12b2c4e36b45
matt <matt@fiddaman.net>
2024-05-06 22:57:41 +0000

    Add util functions for GoCardless payeeName and payeeIban formatting

--- src/app-gocardless/utils.js
+++ src/app-gocardless/utils.js
@@ -1,3 +1,9 @@
+import { title } from './util/title/index.js';
+
+function formatPayeeIban(iban) {
+  return '(' + iban.slice(0, 4) + ' XXX ' + iban.slice(-4) + ')';
+}
+
 export const printIban = (account) => {
   if (account.iban) {
     return '(XXX ' + account.iban.slice(-4) + ')';
@@ -14,3 +20,26 @@ export const sortByBookingDateOrValueDate = (transactions = []) =>
   );
 
 export const amountToInteger = (n) => Math.round(n * 100);
+
+export const formatPayeeName = (trans) => {
+  const nameParts = [];
+
+  const name =
+    trans.debtorName ||
+    trans.creditorName ||
+    trans.remittanceInformationUnstructured ||
+    (trans.remittanceInformationUnstructuredArray || []).join(', ') ||
+    trans.additionalInformation;
+
+  if (name) {
+    nameParts.push(title(name));
+  }
+
+  if (trans.debtorAccount && trans.debtorAccount.iban) {
+    nameParts.push(formatPayeeIban(trans.debtorAccount.iban));
+  } else if (trans.creditorAccount && trans.creditorAccount.iban) {
+    nameParts.push(formatPayeeIban(trans.creditorAccount.iban));
+  }
+
+  return nameParts.join(' ');
+};
