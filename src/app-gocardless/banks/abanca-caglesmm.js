d8a35053a9ce561736c3db7c82e3ff4f2da8ace1
David Carrizo <daca11.n2o@gmail.com>
2024-05-18 12:00:39 +0200

    add missing date and debtorName

--- src/app-gocardless/banks/abanca-caglesmm.js
+++ src/app-gocardless/banks/abanca-caglesmm.js
@@ -4,15 +4,19 @@ import Fallback from './integration-bank.js';
 export default {
   institutionIds: ['ABANCA_CAGLESMM'],
 
+  accessValidForDays: 180,
+
   normalizeAccount(account) {
     return Fallback.normalizeAccount(account);
   },
 
-  // Abanca transactions doesn't get the creditorID properly
+  // Abanca transactions doesn't get the creditorName/debtorName properly
   normalizeTransaction(transaction, _booked) {
     return {
       ...transaction,
       creditorName: transaction.remittanceInformationStructured,
+      debtorName: transaction.remittanceInformationStructured,
+      date: transaction.bookingDate || transaction.valueDate,
     };
   },
 
