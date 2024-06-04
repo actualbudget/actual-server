62e7628b1e92590607c9075420bd9d3a5cd2a0c2
Robert Dyer <rdyer@unl.edu>
2024-06-04 17:51:33 -0500

    Replace loops with flatMap()

--- src/app-gocardless/tests/bank-factory.spec.js
+++ src/app-gocardless/tests/bank-factory.spec.js
@@ -3,15 +3,11 @@ import { banks } from '../bank-factory.js';
 import IntegrationBank from '../banks/integration-bank.js';
 
 describe('BankFactory', () => {
-  for (const bank of banks) {
-    for (const institutionId of bank.institutionIds) {
-      it(`should return institutionId ${institutionId}`, () => {
-        const result = BankFactory(institutionId);
+  it.each(banks.flatMap(bank => bank.institutionIds))(`should return same institutionId`, (institutionId) => {
+    const result = BankFactory(institutionId);
 
-        expect(result.institutionIds).toContain(institutionId);
-      });
-    }
-  }
+    expect(result.institutionIds).toContain(institutionId);
+  });
 
   it('should return IntegrationBank when institutionId is not found', () => {
     const institutionId = IntegrationBank.institutionIds[0];
