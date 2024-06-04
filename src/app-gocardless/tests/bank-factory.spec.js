d617f227ea9de16ca20c5940a99647f8012dc9a9
Robert Dyer <rdyer@unl.edu>
2024-06-04 17:53:10 -0500

    Fix linter

--- src/app-gocardless/tests/bank-factory.spec.js
+++ src/app-gocardless/tests/bank-factory.spec.js
@@ -3,11 +3,14 @@ import { banks } from '../bank-factory.js';
 import IntegrationBank from '../banks/integration-bank.js';
 
 describe('BankFactory', () => {
-  it.each(banks.flatMap(bank => bank.institutionIds))(`should return same institutionId`, (institutionId) => {
-    const result = BankFactory(institutionId);
+  it.each(banks.flatMap((bank) => bank.institutionIds))(
+    `should return same institutionId`,
+    (institutionId) => {
+      const result = BankFactory(institutionId);
 
-    expect(result.institutionIds).toContain(institutionId);
-  });
+      expect(result.institutionIds).toContain(institutionId);
+    },
+  );
 
   it('should return IntegrationBank when institutionId is not found', () => {
     const institutionId = IntegrationBank.institutionIds[0];
