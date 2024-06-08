348687f0a0bacff53b1c6e55fc8bba5af8a89690
David Carrizo <daca11.n2o@gmail.com>
2024-06-08 10:16:13 +0200

    correct linter

--- src/app-gocardless/banks/abanca-caglesmm.js
+++ src/app-gocardless/banks/abanca-caglesmm.js
@@ -3,7 +3,7 @@ import Fallback from './integration-bank.js';
 /** @type {import('./bank.interface.js').IBank} */
 export default {
   ...Fallback,
-  
+
   institutionIds: ['ABANCA_CAGLESMM'],
 
   accessValidForDays: 180,
