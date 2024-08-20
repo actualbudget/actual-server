e58dcbf8e1cd293adf202cee5f938864bf43276e
Robert Dyer <rdyer@unl.edu>
2024-08-20 01:59:48 -0500

    better default error handling

--- src/app-gocardless/util/handle-error.js
+++ src/app-gocardless/util/handle-error.js
@@ -4,8 +4,13 @@ export function handleError(func) {
   return (req, res) => {
     func(req, res).catch((err) => {
       console.log('Error', req.originalUrl, inspect(err, { depth: null }));
-      res.status(500);
-      res.send({ status: 'error', reason: 'internal-error' });
+      res.send({
+        status: 'ok',
+        data: {
+          error_code: 'INTERNAL_ERROR',
+          error_type: err.message ? err.message :  'internal-error',
+        },
+      });
     });
   };
 }
