763cd709e2804a6b2262739a15519101d5af850a
Robert Dyer <rdyer@unl.edu>
2024-08-20 01:34:42 -0500

    have errors print the inspection

--- src/app-gocardless/util/handle-error.js
+++ src/app-gocardless/util/handle-error.js
@@ -1,7 +1,9 @@
+import { inspect } from 'util';
+
 export function handleError(func) {
   return (req, res) => {
     func(req, res).catch((err) => {
-      console.log('Error', req.originalUrl, err);
+      console.log('Error', req.originalUrl, inspect(err, { depth: null }));
       res.status(500);
       res.send({ status: 'error', reason: 'internal-error' });
     });
