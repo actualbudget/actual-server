9ee4421d5c52dbd53301a9ccde2d903420dab6a2
DirgoSalga <diego@dirgosalga.com>
2024-09-15 21:39:58 +0200

    FIx linting errors and add release notes
    Replaced the indentation from 4 spaces to 2, to fix linting error as recommended by the yarn test.
    
    Added the release notes file 454.md.

--- src/app-gocardless/banks/spk_worms_alzey_ried_malade51wor.js
+++ src/app-gocardless/banks/spk_worms_alzey_ried_malade51wor.js
@@ -5,62 +5,62 @@ import { formatPayeeName } from '../../util/payee-name.js';
 
 /** @type {import('./bank.interface.js').IBank} */
 export default {
-    ...Fallback,
+  ...Fallback,
 
-    institutionIds: ['SPK_WORMS_ALZEY_RIED_MALADE51WOR'],
+  institutionIds: ['SPK_WORMS_ALZEY_RIED_MALADE51WOR'],
 
-    accessValidForDays: 90,
+  accessValidForDays: 90,
 
-    normalizeAccount(account) {
-        return {
-            account_id: account.id,
-            institution: account.institution,
-            mask: account.iban.slice(-4),
-            iban: account.iban,
-            name: [account.name, printIban(account)].join(' '),
-            official_name: account.product,
-            type: 'checking',
-        };
-    },
+  normalizeAccount(account) {
+    return {
+      account_id: account.id,
+      institution: account.institution,
+      mask: account.iban.slice(-4),
+      iban: account.iban,
+      name: [account.name, printIban(account)].join(' '),
+      official_name: account.product,
+      type: 'checking',
+    };
+  },
 
-    normalizeTransaction(transaction, _booked) {
-        const date =
-            transaction.bookingDate ||
-            transaction.valueDate;
-        if (!date) {
-            return null;
-        }
+  normalizeTransaction(transaction, _booked) {
+    const date =
+      transaction.bookingDate ||
+      transaction.valueDate;
+    if (!date) {
+      return null;
+    }
 
-        let remittanceInformationUnstructured;
+    let remittanceInformationUnstructured;
 
-        if (transaction.remittanceInformationUnstructured) {
-            remittanceInformationUnstructured =
-                transaction.remittanceInformationUnstructured;
-        } else if (transaction.remittanceInformationStructured) {
-            remittanceInformationUnstructured =
-                transaction.remittanceInformationUnstructured;
-        } else if (transaction.remittanceInformationStructuredArray?.length > 0) {
-            remittanceInformationUnstructured =
-                transaction.remittanceInformationStructuredArray?.join(' ');
-        }
+    if (transaction.remittanceInformationUnstructured) {
+      remittanceInformationUnstructured =
+        transaction.remittanceInformationUnstructured;
+    } else if (transaction.remittanceInformationStructured) {
+      remittanceInformationUnstructured =
+        transaction.remittanceInformationUnstructured;
+    } else if (transaction.remittanceInformationStructuredArray?.length > 0) {
+      remittanceInformationUnstructured =
+        transaction.remittanceInformationStructuredArray?.join(' ');
+    }
 
-        transaction.remittanceInformationUnstructured =
-            remittanceInformationUnstructured;
-        return {
-            ...transaction,
-            payeeName: formatPayeeName(transaction),
-            date: transaction.bookingDate || transaction.valueDate,
-        };
-    },
+    transaction.remittanceInformationUnstructured =
+      remittanceInformationUnstructured;
+    return {
+      ...transaction,
+      payeeName: formatPayeeName(transaction),
+      date: transaction.bookingDate || transaction.valueDate,
+    };
+  },
 
 
-    calculateStartingBalance(sortedTransactions = [], balances = []) {
-        const currentBalance = balances.find(
-            (balance) => 'interimAvailable' === balance.balanceType,
-        );
+  calculateStartingBalance(sortedTransactions = [], balances = []) {
+    const currentBalance = balances.find(
+      (balance) => 'interimAvailable' === balance.balanceType,
+    );
 
-        return sortedTransactions.reduce((total, trans) => {
-            return total - amountToInteger(trans.transactionAmount.amount);
-        }, amountToInteger(currentBalance.balanceAmount.amount));
-    },
+    return sortedTransactions.reduce((total, trans) => {
+      return total - amountToInteger(trans.transactionAmount.amount);
+    }, amountToInteger(currentBalance.balanceAmount.amount));
+  },
 };
