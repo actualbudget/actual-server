87631c57e38d1f9160331916f5d12d9f20a9cc36
hostyn <ruben@martinezhostyn.com>
2024-09-21 09:57:27 +0200

    test: Add RevolutRevolt21 bizum transactions tests

---
+++ src/app-gocardless/banks/tests/revolut_revolt21.spec.js
@@ -0,0 +1,44 @@
+import RevolutRevolt21 from '../revolut_revolt21.js';
+
+describe('RevolutRevolt21', () => {
+  describe('#normalizeTransaction', () => {
+    it('returns the expected remittanceInformationUnstructured from a bizum expense transfer', () => {
+      const transaction = {
+        transactionAmount: { amount: '-1.00', currency: 'EUR' },
+        remittanceInformationUnstructuredArray: [
+          'Bizum payment to: CREDITOR NAME',
+          'Bizum description',
+        ],
+      };
+
+      const normalizedTransaction = RevolutRevolt21.normalizeTransaction(
+        transaction,
+        true,
+      );
+
+      expect(normalizedTransaction.remittanceInformationUnstructured).toEqual(
+        'Bizum description',
+      );
+    });
+  });
+
+  it('returns the expected payeeName and remittanceInformationUnstructured from a bizum income transfer', () => {
+    const transaction = {
+      transactionAmount: { amount: '1.00', currency: 'EUR' },
+      remittanceInformationUnstructuredArray: [
+        'Bizum payment from: DEBTOR NAME',
+        'Bizum description',
+      ],
+    };
+
+    const normalizedTransaction = RevolutRevolt21.normalizeTransaction(
+      transaction,
+      true,
+    );
+
+    expect(normalizedTransaction.payeeName).toEqual('DEBTOR NAME');
+    expect(normalizedTransaction.remittanceInformationUnstructured).toEqual(
+      'Bizum description',
+    );
+  });
+});
