7d1811ebf6c2c2a9e651641eebfe98897cb77913
Fabian Frey <mrsteakhouse1@gmail.com>
2024-11-12 15:26:40 +0100

    feat: use date-fns to parse the transaction date inside transactionId

--- src/app-gocardless/banks/dkb_byladem1.js
+++ src/app-gocardless/banks/dkb_byladem1.js
@@ -1,5 +1,6 @@
 import Fallback from './integration-bank.js';
 import { printIban } from '../utils.js';
+import * as d from 'date-fns';
 
 /** @type {import('./bank.interface.js').IBank} */
 export default {
@@ -22,25 +23,11 @@ export default {
   },
 
   sortTransactions(transactions = []) {
+    const dateFormat = "yyyy-MM-dd-HH.mm.ss.SSSSSS'Z'";
     return transactions.sort((a, b) => {
-      const dateA = transactionIdToDate(a.transactionId);
-      const dateB = transactionIdToDate(b.transactionId);
-      return +dateB - +dateA;
+      const dateA = d.parse(a.transactionId, dateFormat, new Date());
+      const dateB = d.parse(b.transactionId, dateFormat, new Date());
+      return d.compareAsc(dateA, dateB);
     });
   },
 };
-
-/**
- * Takes a transactionId string and converts it into a `Date`.
- *
- * @param {string} transactionId The transactionId in the format `YYYY-MM-DD-HH.MM.SS.NNNNNN`.
- * @returns {Date} The `Date` representation of the given `transactionId`
- */
-function transactionIdToDate(transactionId) {
-  const datePart = /\d{4}-\d{2}-\d{2}/.exec(transactionId)[0];
-  const timePart = /\d{2}\.\d{2}\.\d{2}/
-    .exec(transactionId)[0]
-    .replaceAll('.', ':');
-  const dateTimeString = [datePart, timePart].join(' ');
-  return new Date(dateTimeString);
-}
