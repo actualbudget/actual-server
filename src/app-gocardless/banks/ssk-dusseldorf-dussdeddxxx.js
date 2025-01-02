6014867290877d088a224b4d0f17ae38ec469552
DennaGherlyn <denna@mailbox.org>
2025-01-02 18:27:13 +0100

    adding suggestions

--- src/app-gocardless/banks/ssk-dusseldorf-dussdeddxxx.js
+++ src/app-gocardless/banks/ssk-dusseldorf-dussdeddxxx.js
@@ -1,39 +1,22 @@
 import Fallback from './integration-bank.js';
 
-import { formatPayeeName } from '../../util/payee-name.js';
-
 /** @type {import('./bank.interface.js').IBank} */
 export default {
   ...Fallback,
 
   institutionIds: ['SSK_DUSSELDORF_DUSSDEDDXXX'],
 
-  accessValidForDays: 90,
-
-  /**
-   * Following the GoCardless documentation[0] we should prefer `bookingDate`
-   * here, though some of their bank integrations uses the date field
-   * differently from what's described in their documentation and so it's
-   * sometimes necessary to use `valueDate` instead.
-   *
-   *   [0]: https://nordigen.zendesk.com/hc/en-gb/articles/7899367372829-valueDate-and-bookingDate-for-transactions
-   */
   normalizeTransaction(transaction, _booked) {
-    const date = transaction.bookingDate || transaction.valueDate;
-    if (!date) {
-      return null;
-    }
 
-    let remittanceInformationUnstructured;
-
-    remittanceInformationUnstructured =
+    let remittanceInformationUnstructured =
       transaction.remittanceInformationUnstructured ??
       transaction.remittanceInformationStructured ??
       transaction.remittanceInformationStructuredArray?.join(' ');
 
     if (transaction.additionalInformation)
-      remittanceInformationUnstructured +=
-        ' ' + transaction.additionalInformation;
+      remittanceInformationUnstructured = (remittanceInformationUnstructured ?? '') 
+      + ' ' 
+      + transaction.additionalInformation;
 
     const usefulCreditorName =
       transaction.ultimateCreditor ||
@@ -44,10 +27,6 @@ export default {
     transaction.remittanceInformationUnstructured =
       remittanceInformationUnstructured;
 
-    return {
-      ...transaction,
-      payeeName: formatPayeeName(transaction),
-      date: transaction.bookingDate || transaction.valueDate,
-    };
+    return Fallback.normalizeTransaction(transaction, _booked);
   },
 };
