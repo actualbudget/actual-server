57557e98c955f8b7f3db6a1c76f9641779e799d5
Lenno Nagel <lenno@namespace.ee>
2025-01-08 09:41:45 +0200

    Add support for LHV_LHVBEE22

---
+++ src/app-gocardless/banks/lhv-lhvbee22.js
@@ -0,0 +1,32 @@
+import Fallback from './integration-bank.js';
+
+/** @type {import('./bank.interface.js').IBank} */
+export default {
+  ...Fallback,
+
+  institutionIds: ['LHV_LHVBEE22'],
+
+  accessValidForDays: 90,
+
+  normalizeTransaction(transaction, booked) {
+    // extract bookingDate and creditorName for card transactions, e.g.
+    // (..1234) 2025-01-02 09:32 CrustumOU\Poordi 3\Tallinn\10156     ESTEST
+    // bookingDate: 2025-01-02
+    // creditorName: CrustumOU
+    const cardTxRegex =
+      /^\(\.\.(\d{4})\) (\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}) (.+)$/g;
+    const cardTxMatch = cardTxRegex.exec(
+      transaction?.remittanceInformationUnstructured,
+    );
+
+    if (cardTxMatch) {
+      transaction = {
+        ...transaction,
+        bookingDate: cardTxMatch[2],
+        creditorName: cardTxMatch[4].split('\\')[0].trim(),
+      };
+    }
+
+    return Fallback.normalizeTransaction(transaction, booked);
+  },
+};
