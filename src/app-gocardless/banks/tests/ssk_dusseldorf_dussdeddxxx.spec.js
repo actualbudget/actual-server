f8a0cc3d70b3b459d162419ba0730ce0b532127b
DennaGherlyn <denna@mailbox.org>
2025-01-27 19:21:28 +0100

    Correct consoleSpy to make linter happy

--- src/app-gocardless/banks/tests/ssk_dusseldorf_dussdeddxxx.spec.js
+++ src/app-gocardless/banks/tests/ssk_dusseldorf_dussdeddxxx.spec.js
@@ -1,86 +1,97 @@
 import { jest } from '@jest/globals';
 import SskDusseldorfDussdeddxxx from '../ssk_dusseldorf_dussdeddxxx.js';
 
-beforeEach(() => {
-  jest.spyOn(console, 'debug').mockImplementation(() => {});
-});
+describe('ssk_dusseldorf_dussdeddxxx', () => {
+  let consoleSpy;
 
-afterEach(() => {
-  jest.restoreAllMocks();
-});
-
-describe('#normalizeTransaction', () => {
-  const bookedTransactionOne = {
-    transactionId: '2024102900000000-1',
-    bookingDate: '2024-10-29',
-    valueDate: '2024-10-29',
-    transactionAmount: {
-      amount: '-99.99',
-      currency: 'EUR',
-    },
-    creditorName: 'a useful creditor name',
-    remittanceInformationStructured: 'structured information',
-    remittanceInformationUnstructured: 'unstructured information',
-    additionalInformation: 'some additional information',
-  };
-
-  const bookedTransactionTwo = {
-    transactionId: '2024102900000000-2',
-    bookingDate: '2024-10-29',
-    valueDate: '2024-10-29',
-    transactionAmount: {
-      amount: '-99.99',
-      currency: 'EUR',
-    },
-    creditorName: 'a useful creditor name',
-    ultimateCreditor: 'ultimate creditor',
-    remittanceInformationStructured: 'structured information',
-    additionalInformation: 'some additional information',
-  };
-
-  it('properly combines remittance information', () => {
-    expect(
-      SskDusseldorfDussdeddxxx.normalizeTransaction(bookedTransactionOne, true)
-        .remittanceInformationUnstructured,
-    ).toEqual('unstructured information some additional information');
-
-    expect(
-      SskDusseldorfDussdeddxxx.normalizeTransaction(bookedTransactionTwo, true)
-        .remittanceInformationUnstructured,
-    ).toEqual('structured information some additional information');
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, 'debug');
   });
 
-  it('prioritizes creditor names correctly', () => {
-    expect(
-      SskDusseldorfDussdeddxxx.normalizeTransaction(bookedTransactionOne, true)
-        .payeeName,
-    ).toEqual('A Useful Creditor Name');
+  describe('#normalizeTransaction', () => {
+    const bookedTransactionOne = {
+      transactionId: '2024102900000000-1',
+      bookingDate: '2024-10-29',
+      valueDate: '2024-10-29',
+      transactionAmount: {
+        amount: '-99.99',
+        currency: 'EUR',
+      },
+      creditorName: 'a useful creditor name',
+      remittanceInformationStructured: 'structured information',
+      remittanceInformationUnstructured: 'unstructured information',
+      additionalInformation: 'some additional information',
+    };
 
-    expect(
-      SskDusseldorfDussdeddxxx.normalizeTransaction(bookedTransactionTwo, true)
-        .payeeName,
-    ).toEqual('Ultimate Creditor');
-  });
+    const bookedTransactionTwo = {
+      transactionId: '2024102900000000-2',
+      bookingDate: '2024-10-29',
+      valueDate: '2024-10-29',
+      transactionAmount: {
+        amount: '-99.99',
+        currency: 'EUR',
+      },
+      creditorName: 'a useful creditor name',
+      ultimateCreditor: 'ultimate creditor',
+      remittanceInformationStructured: 'structured information',
+      additionalInformation: 'some additional information',
+    };
 
-  const unbookedTransaction = {
-    transactionId: '2024102900000000-1',
-    valueDate: '2024-10-29',
-    transactionAmount: {
-      amount: '-99.99',
-      currency: 'EUR',
-    },
-    creditorName: 'some nonsensical creditor',
-    remittanceInformationUnstructured: 'some nonsensical information',
-  };
+    it('properly combines remittance information', () => {
+      expect(
+        SskDusseldorfDussdeddxxx.normalizeTransaction(
+          bookedTransactionOne,
+          true,
+        ).remittanceInformationUnstructured,
+      ).toEqual('unstructured information some additional information');
 
-  it('returns null for unbooked transactions', () => {
-    expect(
-      SskDusseldorfDussdeddxxx.normalizeTransaction(unbookedTransaction, false),
-    ).toBeNull();
+      expect(
+        SskDusseldorfDussdeddxxx.normalizeTransaction(
+          bookedTransactionTwo,
+          true,
+        ).remittanceInformationUnstructured,
+      ).toEqual('structured information some additional information');
+    });
 
-    expect(console.debug).toHaveBeenCalledWith(
-      'Skipping unbooked transaction:',
-      unbookedTransaction.transactionId,
-    );
+    it('prioritizes creditor names correctly', () => {
+      expect(
+        SskDusseldorfDussdeddxxx.normalizeTransaction(
+          bookedTransactionOne,
+          true,
+        ).payeeName,
+      ).toEqual('A Useful Creditor Name');
+
+      expect(
+        SskDusseldorfDussdeddxxx.normalizeTransaction(
+          bookedTransactionTwo,
+          true,
+        ).payeeName,
+      ).toEqual('Ultimate Creditor');
+    });
+
+    const unbookedTransaction = {
+      transactionId: '2024102900000000-1',
+      valueDate: '2024-10-29',
+      transactionAmount: {
+        amount: '-99.99',
+        currency: 'EUR',
+      },
+      creditorName: 'some nonsensical creditor',
+      remittanceInformationUnstructured: 'some nonsensical information',
+    };
+
+    it('returns null for unbooked transactions', () => {
+      expect(
+        SskDusseldorfDussdeddxxx.normalizeTransaction(
+          unbookedTransaction,
+          false,
+        ),
+      ).toBeNull();
+
+      expect(consoleSpy).toHaveBeenCalledWith(
+        'Skipping unbooked transaction:',
+        unbookedTransaction.transactionId,
+      );
+    });
   });
 });
